18b419620b94b988a4fcd85e890d08308175dc18
ddiyoung-x4 <ddiyoung-x4@users.noreply.github.com>
2026-09-22 07:41:30 +0000

    deploy: e380f0e60051cf596d87893ec6de5d73e9bd023d

--- assets/ContactPage-BJmid6iu.js
+++ assets/ContactPage-BJmid6iu.js
@@ -1,4 +1,4 @@
-/* empty css                */import{j as e,a,h as c,M as o}from"./index-gMBKB_JP.js";function r(){const t="jongbinryu@ajou.ac.kr",l=encodeURIComponent("MMAI Lab Inquiry"),i=encodeURIComponent(`Hello MMAI Lab,
+/* empty css                */import{j as e,a,h as c,M as o}from"./index-B7wuVmO0.js";function r(){const t="jongbinryu@ajou.ac.kr",l=encodeURIComponent("MMAI Lab Inquiry"),i=encodeURIComponent(`Hello MMAI Lab,
 
 I would like to ask about...
 
