2cd313c883ca44a6123e58be35fe487e24e6474f
ddiyoung-x4 <ddiyoung-x4@users.noreply.github.com>
2026-09-23 07:44:11 +0000

    deploy: 0ace214f43364ca27865698f5754a60fef8edf8f

--- assets/ContactPage-BsQYxac9.js
+++ assets/ContactPage-BsQYxac9.js
@@ -1,4 +1,4 @@
-/* empty css                */import{j as e,a,h as c,M as o}from"./index-afC3S2LA.js";function r(){const t="jongbinryu@ajou.ac.kr",l=encodeURIComponent("MMAI Lab Inquiry"),i=encodeURIComponent(`Hello MMAI Lab,
+/* empty css                */import{j as e,a,h as c,M as o}from"./index-C4uIE4dP.js";function r(){const t="jongbinryu@ajou.ac.kr",l=encodeURIComponent("MMAI Lab Inquiry"),i=encodeURIComponent(`Hello MMAI Lab,
 
 I would like to ask about...
 
